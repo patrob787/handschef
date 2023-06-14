6c66c7f00504c5cae40e168a98c6353deb44b9ca
Patrick Roberts <patrob22@gmail.com>
2023-06-14 14:21:01 -0400

    added schema for User

--- server/models.py
+++ server/models.py
@@ -6,11 +6,22 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from config import db
 
 # Models go here!
+
+#Users table
 class User(db, SerializerMixin):
     __tablename__ = "users"
 
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    avatar = db.Column(db.String)
+    admin = db.Column(db.Boolean, default=False)
+    emp_code = db.Column(db.Integer, unique=True, nullable=False)
+    job_code = db.Column(db.Integer, nullable=False)
+    start_date = db.Column(db.DateTime, server_default=db.func.now())
+
     def __repr__(self):
-        pass
+        return f"<Employee: {self.emp_code}, Name: {self.first_name} {self.last_name}>"
 
 class Item(db, SerializerMixin):
     __tablename__ = "items"
