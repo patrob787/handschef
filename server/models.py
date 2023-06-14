a5ddbd450f8faa7ebbe22427c9dd7e51777e0ae4
Patrick Roberts <patrob22@gmail.com>
2023-06-14 14:08:47 -0400

    added initial declarations for User, Item, Check, and Order

--- server/models.py
+++ server/models.py
@@ -3,3 +3,26 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db
 
 # Models go here!
+class User(db, SerializerMixin):
+    __tablename__ = "users"
+
+    def __repr__(self):
+        pass
+
+class Item(db, SerializerMixin):
+    __tablename__ = "items"
+
+    def __repr__(self):
+        pass
+
+class Check(db, SerializerMixin):
+    __tablename__ = "checks"
+
+    def __repr__(self):
+        pass
+
+class Order(db, SerializerMixin):
+    __tablename__ = "orders"
+
+    def __repr__(self):
+        pass
