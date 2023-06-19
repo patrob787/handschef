39ccb3ac13ea1eeeed1866d0d2b2591a0a627ac0
Patrick Roberts <patrob22@gmail.com>
2023-06-19 15:48:30 -0400

    added Users post and UsersById get, patch and delete

--- server/app.py
+++ server/app.py
@@ -21,12 +21,61 @@ class Users(Resource):
 
         return users, 200
     
-    # def post(self):
-    #     new_user = User(
-    #         first_name = request.form['first_name']
-    #     )
+    def post(self):
+        
+        try:
+            new_user = User(
+                first_name = request.form['first_name'],
+                last_name = request.form['last_name'],
+                avatar = request.form['avatar'],
+                admin = request.form['admin'],
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            return new_user.to_dict(), 201
+        except:
+            return {"error": "400 Validation Error"}, 400
     
 api.add_resource(Users, '/users')
+
+class UserById(Resource):
+    def get(self, id):
+        user = User.query.filter_by(id=id).first()
+
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {"error": "404 User not Found"}, 404
+        
+    def patch(self, id):
+        user = User.query.filter_by(id=id).first()
+
+        if user:
+            for attr in request.json():
+                setattr(user, attr, request.json()[attr])
+            
+            db.session.add(user)
+            db.session.commit()
+
+            return user.to_dict(), 202
+        else:
+            return {"error": "404 User not found"}, 404
+        
+    def delete(self, id):
+        user = User.query.filter_by(id=id).first()
+
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+
+            return {"message": "delete successful"}, 204
+        
+        else:
+            return {"error": "404 User not found"}, 404
+        
+api.add_resource(UserById, '/users/<int:id>')
 
 
 if __name__ == '__main__':
