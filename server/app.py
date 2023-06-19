81954df151fd9a59426e839fb237a40f855b281b
Patrick Roberts <patrob22@gmail.com>
2023-06-19 15:30:58 -0400

    added Users get route

--- server/app.py
+++ server/app.py
@@ -11,6 +11,23 @@ from config import app, db, api
 from models import User, Item, SubItem, Check, Order, Modifier
 
 # Views go here!
+app.route('/')
+def home():
+    return "Welcome to the Handchef API!!"
+
+class Users(Resource):
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+
+        return users, 200
+    
+    # def post(self):
+    #     new_user = User(
+    #         first_name = request.form['first_name']
+    #     )
+    
+api.add_resource(Users, '/users')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
