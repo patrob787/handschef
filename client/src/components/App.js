1ae8ba43fcd3e3ec9e6201be109b722738ee7b81
Patrick Roberts <patrob22@gmail.com>
2023-06-20 14:31:09 -0400

    added user name as well as date and time to header.  changed page background color

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div>
       <MyProvider>
-        <Nav />
+        {/* <Nav /> */}
         <Header />
         
         <Routes>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { MyContext } from './MyProvider'
+import Nav from './Nav'
+import "./App.css"
 
 function Header() {
+  const { user } = useContext(MyContext)
+
+  const [ date, setDate ] = useState(new Date())
+
+  function refreshClock() {
+    setDate(new Date())
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000)
+    return function cleanUp() {
+      clearInterval(timerId)
+    }
+  }, [])
+
   return (
-    <div>
-      <h1>HandsCHEF!!</h1>
+    <div className="header-container">
+      <div className="header-details">
+        <h1>Welcome, {user.first_name.toUpperCase()}</h1>
+        <p>DATE: {date.toLocaleDateString()}</p>
+        <p>TIME: {date.toLocaleTimeString()}</p>
+      </div>
+      <Nav />
     </div>
   )
 }
