f3da572118d40509fd2578ca20d20ec92f09644c
Patrick Roberts <patrob22@gmail.com>
2023-06-28 10:17:59 -0400

    hat only logos added.  Logo added to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,16 +3,21 @@ import { MyContext } from './MyProvider'
 import Nav from './Nav'
 import Clock from './Clock'
 import "./App.css"
+import pinkLogo from "../logos/2.png"
+import pinkHat from "../logos/8.png"
 
 function Header() {
   const { user } = useContext(MyContext)
 
   return (
     <div className="header-container">
-      <div className="header-details">
-        <h1>Welcome, {user.first_name.toUpperCase()}</h1>
-        <p>POSITION: {user.admin ? "Administrator" : "Server"}</p>
-        <Clock />
+      <div>
+        <img id="header-logo" src={pinkHat} />
+        <div className="header-details">
+          <h1>Welcome, {user.first_name.toUpperCase()}</h1>
+          <p>POSITION: {user.admin ? "Administrator" : "Server"}</p>
+          <Clock />
+        </div>
       </div>
       <Nav />
     </div>
